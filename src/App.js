979d0682fd9b233f3642955f36fa78e8e2dcb03f
jonbaxt <jonbaxt@gmail.com>
2018-07-22 20:53:24 -0600

    Setup redux, created a reset sheet to start styling

--- src/App.js
+++ src/App.js
@@ -1,21 +1,47 @@
 import React, { Component } from 'react';
 // import axios from 'axios';
 
+// import ListComp from './ListComp';
+import './reset.css';
 import './App.css';
 
 class App extends Component {
-  constructor() {
-    super()
-    this.state = {
-
-    }
-  }
-
+  // constructor() {
+  //   super()
+  //   this.state = {
+  //     inputStuff: '',
+  //     inputList: ['Dude', 'Word', 'Make My Day'],
+  //   }
+  //   this.handleTextInput = this.handleTextInput.bind(this);
+  //   this.addToList = this.addToList.bind(this);
+  // }
+  // handleTextInput(e) {
+  //   this.setState({
+  //     inputStuff: e.target.value
+  //   })
+  // }
+  // addToList(){
+  //   let newList = this.state.inputList;
+  //   newList.push(this.state.inputStuff)
+  //   // console.log(newList)
+  //   this.setState({
+  //     inputList: newList,
+  //     inputStuff: ''
+  //   })
+  // }
 
   render() {
+    // let createList = this.state.inputList.map( (e, index) => { return (<p key={index} >{e}</p>)})
     return (
       <div className="App">
+        Parental Game Database
 
+        {/* <input type='text' onChange={(e)=> this.handleTextInput(e)} value={this.state.inputStuff} />
+        <button onClick={() => this.addToList()} >Add to the List</button>
+        <h1>{this.state.inputStuff}</h1
+        >
+        <h3>My Stuff in Array</h3>
+        {createList} */}
       </div>
     );
   }
---
+++ src/dux/reducer.js
@@ -0,0 +1,15 @@
+
+
+const initialState = {
+    games: [],
+
+}
+
+export default function reducer(state = initialState, action) {
+    switch (action.type) {
+
+        //----------------------------------DEFAULT RETURN----------------------------------
+        default:
+            return state;
+    }
+}
